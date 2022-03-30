43ad9d7935010ddde714076e86515bf80e455712
Dylan <user@users-MBP-2.broadband>
2022-03-30 23:45:57 +0100

    updating models index file

--- models/index.js
+++ models/index.js
@@ -7,3 +7,14 @@ User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "CASCADE"
+});
+
