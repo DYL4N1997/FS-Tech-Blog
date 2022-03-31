30704dc69eb132cabd294f45e329f4ebdb03f99a
Dylan <user@users-MBP-2.broadband>
2022-03-31 21:56:00 +0100

    updating models post file

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class Post extends Model {}
 
-Project.init(
+Post.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING(30),
+      unique: true,
+      allowNull: false,
+    }, 
+    post_data {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    
 
 
 module.exports = Project;
