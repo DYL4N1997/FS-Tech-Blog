82051814a78f6b550da831a0d4a6e0f3e12a13e8
Dylan <user@users-MBP-2.broadband>
2022-04-01 23:47:50 +0100

    Updating commentroutes controller

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -20,9 +20,37 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 // // Route to POST a comment
-// router.post('/', withAuth, async (req, res) => {
-//     const
-// })
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
 
+        res.status(200).json(newComment);
+      } catch (err) {
+          res.status(500).json(err.message);
+      }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment waas found relating to the ID'});
+            return;
+        } 
+
+       res.status(200).json(commentData);
+    }  catch (err) {
+        res.status(500).json(err.message);
+    } 
+});
 
 module.exports = router;
