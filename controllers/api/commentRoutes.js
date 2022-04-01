61d356a000d1d1b1f31740e5f4071fd8358e0935
Dylan <user@users-MacBook-Pro-2.local>
2022-04-01 15:51:35 +0100

    updating comment routes, findAll

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,28 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+// Get all available comments
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            include: [User],
+        });
+
+        const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+        console.log(comments);
+
+        res.render('single-post', { comments, logged_in: req.session.logged_in});
+      } catch (err) {
+          res.status(500).json(err.message);
+      }
+});
+
+// // Route to POST a comment
+// router.post('/', withAuth, async (req, res) => {
+//     const
+// })
 
 
 module.exports = router;
