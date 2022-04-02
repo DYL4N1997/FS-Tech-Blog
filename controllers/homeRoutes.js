d408e53382b42f199d7fac77f9d93a2039433bfb
Dylan <user@users-MBP-2.broadband>
2022-04-02 13:26:27 +0100

    Finding one post route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,8 +18,36 @@ router.get('/', async (req, res) => {
     // Serialization of the data
     const posts = postData.map((post) => post.get({ plain: true }));
     // Rendering of posts & location
-    res.render('homepage-admin', { posts, logged_in: req.session.logged_in });
+    res.render('allposts-loggedin', { posts, logged_in: req.session.logged_in });
   } catch (err)  {
     res.status(500).json(err.message);
   }
+});
+
+// Gets a single post, including the comments that are assigned to it
+router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+          include: [
+            {
+                model: User,
+                attributes: ['username'], 
+            },
+            {
+                model: Comment,
+                include: [User],
+            },
+          ],
+        });
+
+        if (postData) {
+        const post = postData.get ({ plain: true });
+        res.render('single-post', { post, logged_in: req.session.logged_in });
+      } else {
+          res.status(404).end();
+          console.log('session has ended, try to login again');
+        }
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
 });
