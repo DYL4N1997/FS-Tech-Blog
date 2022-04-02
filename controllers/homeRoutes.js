296d1131eda242513f1a0fdb24911bdafdbe80b9
Dylan <user@users-MBP-2.broadband>
2022-04-02 12:03:39 +0100

    Configuring home routes for post data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,3 +3,23 @@ const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Get all posts
+router.get('/', async (req, res) => {
+    try {
+    //   Get all post data, including the User
+     const postData = await Post.findAll({
+         include: [
+            { 
+             model: User,
+             attributes: ['username'],
+            },
+         ],
+    });
+
+    // Serialization of the data
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // Rendering of posts & location
+    res.render('homepage-admin', { posts, logged_in: req.session.logged_in });
+  } catch (err)  {
+    res.status(500).json(err.message);
+  }
+});
