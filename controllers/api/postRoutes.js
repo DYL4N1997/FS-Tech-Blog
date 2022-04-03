dbeaa3183448684d6241d5c4a7c9fe9dc3982928
Dylan <user@users-MBP-2.broadband>
2022-04-03 19:59:52 +0100

    Route for deleting  a post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -38,6 +38,24 @@ router.put('/:id', withAuth, async (req, res) => {
    }
 });
 
+// Delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+    const postData = await Post.destroy({
+     where: { id: req.params.id, user_id: req.session.user_id, },
+    });
+
+    if (!postData) {
+     res.status(404).json({ message: 'No posts have been found under this ID' });
+     return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
 
 
 module.exports = router;
