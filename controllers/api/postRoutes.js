9a722f1512f80330fa5b62431634cb65cb98a7fa
Dylan <user@users-MBP-2.broadband>
2022-04-03 19:27:50 +0100

    getting all posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
-const { Project, User } = require('../../models');
+const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+// Get all posts
+router.get('/', async (req, res) => {
+    try {
+     const postData = await Post.findAll({});
+
+     res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+});
+
 
 
 module.exports = router;
