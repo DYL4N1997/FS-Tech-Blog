7e894e4f3bfaa95c66ce9333b330e693aef99264
Dylan <user@users-MBP-2.broadband>
2022-04-03 11:28:19 +0100

    Sorting server issue

--- seeds/seed.js
+++ seeds/seed.js
@@ -13,6 +13,16 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
+  
   process.exit(0);
 };
 
